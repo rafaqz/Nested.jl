9a97ae531cea95542552237a63660740bd66b9af
Rafael Schouten <rafaelschouten@gmail.com>
2018-08-11 14:40:00 +1000

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,5 +5,24 @@ else
     using Test
 end
 
-# write your own tests here
-@test 1 == 2
+flatten_expr(T, path, x) = :(flatten(getfield($path, $(QuoteNode(x)))) 
+flatten_inner(T) = nested(T, :t, flatten_expr, down)
+
+flatten(x::Any) = (x,) 
+flatten(x::Number) = (x,) 
+@generated flatten(t) = flatten_inner(t)
+
+struct Foo{T}
+    a::T
+    b::T
+    c::T
+end
+
+struct NestedFoo{T1, T2}
+    nf::Foo{T1}
+    nb::T2
+    nc::T2
+end
+
+nestedfoo = NestedFoo(Foo(1,2,3),4,5)
+@test flatten(nestedfoo) == (1, 2, 3, 4, 5)
