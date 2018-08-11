d31efb8bf3325bf608b3783e9060a6260e16dca7
Rafael Schouten <rafaelschouten@gmail.com>
2018-08-11 15:27:13 +1000

    bugfix

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,8 +8,6 @@ end
 flatten_expr(T, path, x) = :(flatten(getfield($path, $(QuoteNode(x)))))
 flatten_inner(T) = nested(T, :t, flatten_expr, down)
 
-flatten_inner(typeof(foo))
-
 flatten(x::Any) = (x,)
 flatten(x::Number) = (x,)
 @generated flatten(t) = flatten_inner(t)
@@ -29,4 +27,4 @@ end
 nestedfoo = NestedFoo(Foo(1,2,3),4,5)
 tuplefoo = Foo(6.0, 7.0, (8.0, 9.0, 10.0))
 @test flatten(nestedfoo) == (1, 2, 3, 4, 5)
-@test flatten((nestedfoo, uplefoo)) == (1, 2, 3, 4, 5, 6.0, 7.0, 8.0, 9.0, 10.0)
+@test flatten((nestedfoo, tuplefoo)) == (1, 2, 3, 4, 5, 6.0, 7.0, 8.0, 9.0, 10.0)
