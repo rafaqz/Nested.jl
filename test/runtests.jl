e9c1be1207c20744cdb2c06ad86d921595d2931d
Rafael Schouten <rafaelschouten@gmail.com>
2018-08-11 15:17:59 +1000

    test tuples

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,17 +12,19 @@ flatten(x::Any) = (x,)
 flatten(x::Number) = (x,) 
 @generated flatten(t) = flatten_inner(t)
 
-struct Foo{T}
-    a::T
-    b::T
-    c::T
+struct Foo{T1,T2,T3}
+    a::T1
+    b::T2
+    c::T3
 end
 
-struct NestedFoo{T1, T2}
-    nf::Foo{T1}
-    nb::T2
-    nc::T2
+struct NestedFoo{T1,T2,T3,T4,T5}
+    nf::Foo{T1,T2,T3}
+    nb::T4
+    nc::T5
 end
 
 nestedfoo = NestedFoo(Foo(1,2,3),4,5)
+tuplefoo = Foo(6.0, 7.0, (8.0, 9.0, 10.0))
 @test flatten(nestedfoo) == (1, 2, 3, 4, 5)
+@test flatten((nestedfoo, tuplefoo)) == (1, 2, 3, 4, 5, 6.0, 7.0, 8.0, 9.0, 10.0)
