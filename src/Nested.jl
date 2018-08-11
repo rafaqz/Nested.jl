6ac937c19e26ce2d0931637c3a09c6d01e30dd74
Rafael Schouten <rafaelschouten@gmail.com>
2018-08-11 20:38:14 +1000

    update docs

--- src/Nested.jl
+++ src/Nested.jl
@@ -7,18 +7,18 @@ using Compat
 export nested 
 
 """
-    nested(T::Type, dir::Union{Up,Down}, path::Union{Symbol,Expr}, check::Union{Symbol,Expr}, val, alt)
-
+    nested(T::Type, path, expr_builder, handler=default_handler) 
 Builds an arbitrary expression from each nested field in the passed in object.
 
 Arguments:
 - `T`: the type of the current object
 - `P`: the type of the parent object
 - `path`: a symbol or expression containing the `.` path from the original type to the current object.
+- `expr_builder`: function that returns an expression given T, path and a field name or index
 - `handler`: function that handles the collected expressions
 """
-
-nested(T::Type, path, expr_builder, handler=default_handler) = nested(T, Nothing, path, expr_builder, handler)
+nested(T::Type, path, expr_builder, handler=default_handler) = 
+    nested(T, Nothing, path, expr_builder, handler)
 nested(T::Type, P::Type, path, expr_builder, handler) = begin
     expressions = []
     for fname in fieldnames(T)
