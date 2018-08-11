b3643ae3b17a46e7f89a3c9f0828a0d834e9bccd
Rafael Schouten <rafaelschouten@gmail.com>
2018-08-11 14:20:08 +1000

    refactor and simplify

--- README.md
+++ README.md
@@ -11,17 +11,77 @@ user facing functionality.
 
 See [Flatten.jl](https://github.com/rafaqz/Flatten.jl) for an implementation.
 
-Concept:
-- Functions produced should be type stable and very fast
-- nested checks if a field is to be included _after_ the generated function runs,
-  so you can override methods. But still at compile time, so it has no runtime cost. 
-- `nested()` runs in `Down` or `Up` mode - flattening things or constructing things
-- Tuples and Structs are splatted, or constructed
-- Voids are removed, or reinserted 
-- Units are stripped, or reapplied
-- Numbers and Any are processes with the passed in `val` function
-- Nested does not currently deconstruct arrays, as these are more difficult to
-  handle at compile time.
-- Other types are yet to be decided. 
+Prosses:
+- Provide the inner expression that at some point calls your generated function
+  again.
+- Provide some methods for particular types that return a value. These can be
+  overriden at any time as they are not actually inside a generated function.
+- Wrap all method results in a tuple. This allows empty results to be splatted away,
+  and conveniently returns single fields in the same format as structs and
+  tuples.
+- Provide an @generated function that calls nested.
+- Choose a handler: up constructs things, down flattens things. You can also
+  provide your own handler function if the default up/down functions arenet enough.
 
-You can't write your own methods, this is all hyperpure for `@generated`.
+Done. Functions produced should be type stable and _very_ fast
+
+A simple example that flattens nested structures to tuples:
+
+```julia
+flatten_expr(T, path, i::Int) = :(flatten($path[$i]))
+flatten_expr(T, path, fname::Symbol) = :(flatten($path.$fname))
+
+flatten_inner(T) = nested(T, :t, flatten_expr, down)
+
+flatten(x::Any) = (x,) 
+flatten(x::Number) = (x,) 
+@generated flatten(t) = flatten_inner(t)
+```
+
+Test it out:
+
+```julia
+struct Foo{T}
+    a::T
+    b::T
+    c::T
+end
+
+struct NestedFoo{T1, T2}
+    nf::Foo{T1}
+    nb::T2
+    nc::T2
+end
+
+julia> nestedfoo = NestedFoo(Foo(1,2,3),4,5)
+NestedFoo{Int64,Int64}(Foo{Int64}(1, 2, 3), 4, 5)
+
+julia> flatten(nestedfoo)
+(1, 2, 3, 4, 5)
+```
+
+Performance:
+
+```julia
+julia> using BenchmarkTools
+julia> @btime flatten($nestedfoo)                                                                                      
+  1.074 ns (0 allocations: 0 bytes)                                                                                    
+(1, 2, 3, 4, 5)  
+
+julia> @code_native flatten(nestedfoo)                                                                                 
+        .text                                                                                                          
+Filename: REPL[7]
+        pushq   %rbp
+        movq    %rsp, %rbp
+Source line: 1
+        movups  (%rsi), %xmm0
+        movups  16(%rsi), %xmm1
+        movq    32(%rsi), %rax
+        movups  %xmm0, (%rdi)
+        movups  %xmm1, 16(%rdi)
+        movq    %rax, 32(%rdi)
+        movq    %rdi, %rax
+        popq    %rbp
+        retq
+        nop
+```
--- src/Nested.jl
+++ src/Nested.jl
@@ -2,94 +2,39 @@ __precompile__()
 
 module Nested
 
-using Unitful
-
-export Include, Exclude, Up, Down, Strip, Leave, nested, nested_include_all, nested_alt, nested_wrap, nested_val
-
-# Stopgap singletons until boolean constants work, in 0.7
-struct Include end
-struct Exclude end
-
-struct Up end
-struct Down end
-
-struct Strip end
-struct Leave end
-
-"Simple check functioon that accepts all fields"
-nested_include_all(x, field) = Include()
-"Simple val function that simply returns the path"
-nested_val(T, P, path) = path
-"Simple alt function that inserts an empty tuple if the check function fails, to later be splatted out of existence"
-nested_alt(path, fname) = ()
+export nested, up, down 
 
 """
     nested(T::Type, dir::Union{Up,Down}, path::Union{Symbol,Expr}, check::Union{Symbol,Expr}, val, alt)
-Builds an arbitrary expression built from each nested field in the passed in object.
+
+Builds an arbitrary expression from each nested field in the passed in object.
 
 Arguments:
 - `T`: the type of the current object
 - `P`: the type of the parent object
 - `path`: a symbol or expression containing the `.` path from the original type to the current object.
-
-Optional Arguments:
-- `check`: a symbol or expression for the function that checks if a field should be included.
-- `val`: a function that returns the expression that gives the value for the field
-   this function takes two arguments: struct type and fieldname.
-- `alt`: a function that returns an alternate value if the field is not to be included
-- `units`: Strip() or Leave()
+- `handler`: function that handles the collected expressions
 """
-nested(T::Type, 
-       dir, 
-       path, 
-       check = :nested_include_all, 
-       val = nested_val, 
-       alt = nested_alt,
-       units = Strip()) = nested(T, Void, dir, path, check, val, alt, units)
 
-nested(T::Type, P::Type, dir, path, check, val, alt, units) = begin
-    fnames = fieldnames(T)
+nested(T::Type, path, exprbuilder, handler) = nested(T, Void, path, exprbuilder, handler)  
+nested(T::Type, P::Type, path, exprbuilder, handler) = begin
     expressions = []
-    for (i, fname) in enumerate(fnames)
-        expr = :(
-            if $check($T, $(Expr(:curly, :Val, QuoteNode(fnames[i])))) == Include()
-                $(nested(fieldtype(T, i), T, dir, Expr(:., path, Expr(:quote, fname)), 
-                         check, val, alt, units))
-            else
-                $(alt(path, fnames[i]))
-            end
-        )
-        push!(expressions, Expr(:..., expr))
+    for fname in fieldnames(T)
+        push!(expressions, Expr(:..., exprbuilder(T, path, fname))) 
     end
-    structwrap(T, dir, expressions)
+    handler(T, expressions)
 end
-nested(::Type{T}, P::Type, dir, path, args...) where T <: Tuple = begin
+nested(::Type{<:Tuple}, path, handler) = begin
     expressions = []
     for i in 1:length(T.types)
-        expr = nested(fieldtype(T, i), T, dir, Expr(:ref, path, i), args...)
-        push!(expressions, Expr(:..., expr))
+        push!(expressions, Expr(:..., exprbuilder(T, path, i)))
     end
-    tuplewrap(T, dir, expressions)
+    handler(T, expressions)
 end
-nested(::Type{T}, P::Type, ::Down, args...) where T <: Void = Expr(:tuple)
-nested(::Type{T}, P::Type, ::Up, args...) where T <: Void = Expr(:tuple, nothing) 
-nested(::Type{T}, P::Type, ::Down, path, check, val, alt, units::Strip) where T <: Unitful.Quantity = 
-    Expr(:tuple, val(T, P, Expr(:., path, QuoteNode(:val))))
-nested(::Type{T}, P::Type, ::Up, path, check, val, alt, units::Strip) where T <: Unitful.Quantity = 
-    Expr(:call, :*, Unitful.unit(T), val(T, P, path))
-nested(::Type{T}, P::Type, dir, path, check, val, alt, units::Leave) where T <: Unitful.Quantity = 
-    Expr(:tuple, val(T, P, path))
-nested(::Type{Any}, P::Type, dir, path, check, val, args...) = 
-    Expr(:tuple, val(Any, P, path))
-nested(::Type{T}, P::Type, dir, path, check, val, args...) where T <: Number = 
-    Expr(:tuple, val(T, P, path))
 
-tuplewrap(T, ::Up, expressions) = Expr(:tuple, Expr(:tuple, expressions...))
-tuplewrap(T, ::Down, expressions) = Expr(:tuple, expressions...)
+up(T, expressions) = Expr(:tuple, Expr(:call, :($T.name.wrapper), expressions...))
+up(T::Tuple, expressions) = Expr(:tuple, Expr(:tuple, expressions...))
 
-structwrap(T, ::Up, expressions) = 
-    Expr(:tuple, Expr(:call, Expr(:., Expr(:., T, QuoteNode(:name)), QuoteNode(:wrapper)), expressions...))
-structwrap(T, ::Down, expressions) = Expr(:tuple, expressions...)
-
+down(T, expressions) = Expr(:tuple, expressions...)
 
 end # module
